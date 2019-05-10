62115d7364764c8b447cbd2006670970b53e3f5c
Debajyoti Chatterjee <dchatterjee172@gmail.com>
2019-05-10 10:05:46 +0530

    WIP setup benchmark properly

--- bench.py
+++ bench.py
@@ -24,10 +24,10 @@ def build(df):
 
 df = pd.DataFrame(list(v1))
 df = df.loc[~df.is_train]
-df, ptoi, itop, passages = build(df)
 df = df.sample(df.shape[0])
 df = df.reset_index()
 df = df[:10]  # keep it small for now
+df, ptoi, itop, passages = build(df)
 all_text = "".join(text_to_words(" ".join(df.context).lower()))
 all_letters = [letter for letter, count in Counter(all_text).most_common(50)]
 print(all_letters)
@@ -38,10 +38,12 @@ docs = list(set(df.context))
 x = vec.fit_transform(docs)
 qv = vec.transform(df.question)
 result = pd.np.argmax(pd.np.einsum("kd,md->km", x, qv), axis=0)
-print(result)
+print("charidf")
+print(result, df.ctxid.values)
 
 tfidf = TfidfVectorizer()
 x = tfidf.fit_transform(docs).todense()
 qv = tfidf.transform(df.question).todense()
 tf_result = pd.np.argmax(pd.np.einsum("kd,md->km", x, qv), axis=0)
-print(tf_result)
+print("tfidf")
+print(tf_result, df.ctxid.values)
