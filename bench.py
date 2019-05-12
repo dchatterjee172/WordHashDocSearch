5068daa47097960f03a922b720ac2d683dfd7166
Debajyoti Chatterjee <dchatterjee172@gmail.com>
2019-05-12 14:16:03 +0530

    trying to make the algo better

--- bench.py
+++ bench.py
@@ -2,8 +2,6 @@ import pandas as pd
 from squad_df import v1
 from sklearn.feature_extraction.text import TfidfVectorizer
 from word_hash import CharIdf
-from collections import Counter
-from blingfire import text_to_words
 
 
 def build(df):
@@ -20,17 +18,13 @@ df = pd.DataFrame(list(v1))
 df = df.loc[~df.is_train]
 df = df.sample(df.shape[0])
 df = df.reset_index()
-df = df[:1000]  # keep it small for now
+df = df[:100]  # keep it small for now
 df = build(df)
-all_text = "".join(text_to_words(" ".join(df.context).lower()))
-all_letters = [letter for letter, count in Counter(all_text).most_common(50)]
-print(all_letters)
-print(f"{len(all_letters)} letters")
 
-vec = CharIdf(all_letters)
+vec = CharIdf()
 docs = list(set(df.context))
 x = vec.fit_transform(docs)
-qv = vec.transform(df.question)
+qv = vec.transform(df.question.values)
 result = pd.np.argmax(pd.np.einsum("kd,md->km", x, qv), axis=0)
 print("charidf")
 print(pd.np.mean(result == df.ctxid.values))
--- word_hash.py
+++ word_hash.py
@@ -2,27 +2,29 @@ import numpy as np
 from collections import Counter, defaultdict
 from blingfire import text_to_words
 from tqdm import tqdm
-from itertools import permutations
 
 
 class CharIdf:
-    def __init__(self, all_letters, ngrams=2, tokenizer=text_to_words):
+    def __init__(self, ngrams=4, tokenizer=text_to_words):
         self.ngrams = ngrams
-        self.all_letters = list(set(all_letters))
         self.grams = []
-        for i in range(1, ngrams + 1):
-            self.grams += ["".join(i) for i in permutations(self.all_letters, i)]
-        self.gram_length = len(self.grams)
-        self.gram_to_index = {gram: index for index, gram in enumerate(self.grams)}
+        self.gram_to_index = dict()
         self.tokenizer = tokenizer
+        self._fit = False
+
+    @property
+    def gram_length(self):
+        return len(self.grams)
 
     def _make_grams(self, word):
         "Make char n-grams from words"
         # skip those chars which you know nothing about
-        word = [i for i in word if i in self.all_letters]
         for i in range(len(word)):
-            for j in range(i, i + self.ngrams):
-                yield "".join(word[i : j + 1])
+            for j in range(i + self.ngrams - 1, i + self.ngrams + 1):
+                yield word[i:j]
+            yield word
+
+        # yield word
 
     def __getitem__(self, word, cache={}):
         "Get a word's vector"
@@ -36,16 +38,24 @@ class CharIdf:
 
     def fit(self, docs):
         "Learn idfs"
+        self._fit = True
         self.idf = defaultdict(int)
         for doc in docs:
-            for word in set(self.tokenizer(doc)):
+            for word in self.tokenizer(doc).split():
                 self.idf[word] += 1
+                for gram in self._make_grams(word):
+                    if gram not in self.gram_to_index:
+                        self.gram_to_index[gram] = len(self.grams)
+                        self.grams.append(gram)
+        print(self.grams)
 
     def transform(self, docs):
         "Get vectors for list of strings"
+        if not self._fit:
+            raise Exception("fit first")
         docvecs = np.zeros((len(docs), self.gram_length))
         for index, doc in enumerate(tqdm(docs)):
-            for word, count in Counter(self.tokenizer(doc)).items():
+            for word, count in Counter(self.tokenizer(doc).split()).items():
                 v = (self[word] * count) / (1 + self.idf[word])
                 docvecs[index] += v
         return docvecs
@@ -53,11 +63,3 @@ class CharIdf:
     def fit_transform(self, docs):
         self.fit(docs)
         return self.transform(docs)
-
-
-if __name__ == "__main__":
-    vec = CharIdf()
-    docs = list(set(df.context))
-    x = vec.fit_transform(docs)
-    qv = vec.transform(df.question)
-    result = np.einsum("kd,md->km", x, qv)
